f375ac3223fefb3961be71f0b44f51ae8e116cb0
Madhu sudan Bhattarai <summerpalpali@gmail.com>
2023-08-19 19:28:19 +0545

    Add project files.

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,122 @@
+﻿using EmployeeDataAccess;
+using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeWebApi.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public HttpResponseMessage Get(string gender = "All")
+        {
+            EmpolyeeDBEntities entities = new EmpolyeeDBEntities();
+            {
+                switch (gender.ToLower())
+                {
+                    case "all":
+                        return Request.CreateResponse(HttpStatusCode.OK, entities.Employees.ToList());
+                    case "male":
+                        return Request.CreateResponse(HttpStatusCode.OK, entities.Employees.Where(e => e.Gender.ToLower() == "male").ToList());
+                    case "female":
+                        return Request.CreateResponse(HttpStatusCode.OK, entities.Employees.Where(e => e.Gender.ToLower() == "female").ToList());
+                    default:
+                        return Request.CreateResponse(HttpStatusCode.OK,
+                            "Value for gender must be male, femal or all." + gender + " is valid");
+                }
+            }
+        }
+        public HttpResponseMessage Get(int id)
+        {
+            EmpolyeeDBEntities entities = new EmpolyeeDBEntities();
+            {
+                var entity = entities.Employees.FirstOrDefault(e => e.Id == id);
+                if (entity != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, entity);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, "Employee with Id " + id.ToString() + " not found");
+                }
+            }
+        }
+
+        public HttpResponseMessage Post(Employee employee)
+        {
+            try
+            {
+                EmpolyeeDBEntities entities = new EmpolyeeDBEntities();
+                {
+                    entities.Employees.Add(employee);
+                    entities.SaveChanges();
+
+                    var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+                    message.Headers.Location = new Uri(Request.RequestUri + employee.Id.ToString());
+                    return message;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+        public HttpResponseMessage Put(int id, Employee employee)
+        {
+            try
+            {
+
+                EmpolyeeDBEntities entities = new EmpolyeeDBEntities();
+                {
+                    var entity = entities.Employees.FirstOrDefault(e => e.Id == id);
+                    if (entity == null)
+                    {
+                        return Request.CreateResponse(HttpStatusCode.NotFound, "Employee with Id " + id.ToString() + " not found");
+
+                    }
+                    else
+                    {
+                        entity.FirstName = employee.FirstName;
+                        entity.LastName = employee.LastName;
+                        entities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK, entity);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                EmpolyeeDBEntities entities = new EmpolyeeDBEntities();
+                {
+                    var entity = entities.Employees.FirstOrDefault(e => e.Id == id);
+                    if (entity == null)
+                    {
+                        return Request.CreateResponse(HttpStatusCode.NotFound, "Employee with Id " + id.ToString() + " not found to delete");
+                    }
+                    else
+                    {
+                        entities.Employees.Remove(entity);
+                        entities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK, entity);
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+    }
+}
